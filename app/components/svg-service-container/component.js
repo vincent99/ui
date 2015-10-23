2dff5b5284752985666baeda54bcaa5dfd1e147c
Vincent Fiduccia <vincent@rancher.com>
2015-10-22 20:39:21 -0700

    Working foreignObjects and other random changes

--- app/components/svg-service-container/component.js
+++ app/components/svg-service-container/component.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: '',
-  xCoord: Ember.computed('service.coords', function() {
-    return this.get('service.coords')[0];
+  node: null,
+
+
+  tagName: 'g',
+  attributeBindings: ['transform'],
+
+  width: 235,
+  height: 66,
+
+  transform: Ember.computed('node.{x,y}', function() {
+    return `translate(${this.get('node.x')},${this.get('node.y')})`;
   }),
-  yCoord: Ember.computed('service.coords', function() {
-    return this.get('service.coords')[1];
-  })
 });
--- app/components/svg-service-graph/component.js
+++ app/components/svg-service-graph/component.js
@@ -1,14 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: '',
-  grid: [],
-  setup: function() {
-    this.buildLayout();
-  }.on('init'),
-  buildLayout: function() {
-    this.get('services').forEach((item, index, enumerable) => {
-      item.set('coords', [Math.round(Math.random() * 1000), Math.round(Math.random() * 1000)]);
+  services: null,
+
+  tagName: 'svg',
+  attributeBindings: ['width','height','xmlns','viewBox'],
+
+  width:"100%",
+  height:"100%",
+  xmlns:"http://www.w3.org/2000/svg",
+  viewBox: '0 0 1000 1000',
+
+  nodes: Ember.computed('services.[]', function() {
+    return this.get('services').map((service, index) => {
+      return {
+        x: 100 + 300*(index%3),
+        y: 100*Math.floor(index/3),
+        service: service
+      };
     });
-  }
+  }),
 });
